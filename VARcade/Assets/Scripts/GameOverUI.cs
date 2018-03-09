45721ff85b94d8dfc2ed58391893cfed6a2ccfff
John2509 <jmarcelofo@gmail.com>
2018-03-09 03:39:09 -0300

    Implementacao da IU de Gameover

---
+++ VARcade/Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour {
+
+    PlayerLife playerLife;
+    bool GameOver;
+
+    private void Awake()
+    {
+        playerLife = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerLife>();
+        GameOver = false;
+    }
+
+    void Update ()
+    {
+		if (!playerLife.IsAlive() && !GameOver)
+        {
+            GetComponent<Canvas>().enabled = true;
+            GameOver = true;
+        }
+	}
+}
